1372433444a98e68d3995a7f4cef46029708365b
James Gregory <james.fractal@protonmail.com>
2020-06-25 10:46:30 +1000

    create app file with test route

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return ('hello world')
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    ip = os.environ.get('IP', '127.0.0.1')
+    app.run(host=ip, port=port, debug=True)
