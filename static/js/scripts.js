b4da5ccd27d3de2d90d504ab82ea2e94cf136053
James Gregory <james.fractal@protonmail.com>
2020-07-07 12:57:17 +1000

    move adding favourite border inside doc ready, now works as expected

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,12 @@
 /*jshint esversion: 10 */
 /*jshint -W033 */
 
-$('.is_favourite').on('load', () => {
-	$('.is_favourite').addClass('favourite_border')
-})
-
 $(document).ready(function () {
+
+	$('.is_favourite').addClass('favourite_border')
 
 	$(document).click(function() {
 		$('.navbar-collapse').collapse('hide')
 	})
 
 })
-
-
