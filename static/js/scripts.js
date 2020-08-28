00a8b4b2b4cd3cbf8d9f0a2e3acf1f777e972f50
James Gregory <james.fractal@protonmail.com>
2020-08-28 11:08:28 +1000

    clean up code for api query and filtering

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,30 +1,39 @@
 // prettier-ignore
-heroIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129]
+const heroIdList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129]
 // prettier-enable
+const heroFilterTitle = $(".hero-filter-title")
+const heroFilterButton = $(".js-filter-button")
 
-$(".toast").toast("show")
-
-$(".toast-close").click(() => {
-	$(".toast").toast("hide")
-})
-
+/**
+ * Sends a query to server to filter heroes by attribute.
+ * @async
+ * @param {string} query the attribute to filter by
+ * @returns {object} JSON data from the response
+ */
 const queryApi = async (query) => {
-	const queryUrl = `/api/?attr=${query}`
+	const queryUrl = `/api/heroes/?attr=${query}`
 	const res = await fetch(queryUrl)
-	data = await res.json()
-	return data
+	return await res.json()
 }
 
+/**
+ * Processes the data from the response and creates and array from the values. Then applies filter to relevant elements.
+ * @async
+ * @param {string} query the attribute to filter by
+ */
 const processData = async (query) => {
-	data = await queryApi(query)
+	const data = await queryApi(query)
 	const dataValues = []
 	for (const id of Object.values(data)) {
 		dataValues.push(id)
 	}
-	console.log(dataValues)
-	return dataValues
+	filterHeroes(dataValues)
 }
 
+/**
+ * Hides all heroes and then shows matching heroes.
+ * @param {array} heroes array of hero IDs to filter
+ */
 const filterHeroes = (heroes) => {
 	$(".hero-thumbnail").hide()
 	heroes.forEach((id) => {
@@ -32,23 +41,55 @@ const filterHeroes = (heroes) => {
 	})
 }
 
-document.querySelectorAll(".js-filter-button").forEach((element) => {
-	element.addEventListener("click", async function () {
-		console.log(this.id)
-		query = this.id
+/**
+ * Clears hero filter to show full list.
+ */
+const clearFilter = () => {
+	$(".hero-thumbnail").show()
+	_this.classList.remove("hero-filter-active")
+	heroFilterTitle.text("").removeClass("mb-3")
+}
 
-		filteredHeroes = await processData(query)
-		filterHeroes(filteredHeroes)
+/**
+ * Adds click event to filter elements, creates a query parameter and updates DOM.
+ */
+document.querySelectorAll(".js-filter-button").forEach((element) => {
+	element.addEventListener("click", function () {
+		_this = this
+		if (this.classList.contains("hero-filter-active")) {
+			clearFilter()
+		} else {
+			heroFilterButton.removeClass("hero-filter-active")
+			query = this.id
+			this.classList.add("hero-filter-active")
+			heroFilterTitle.text(`${this.id}`).addClass("mb-3")
+			processData(query)
+		}
 	})
 })
 
-heroIds.forEach((id) => {
-	$(`#${id}-toggler`).click(() => {
-		$(`#${id}`).toggle()
+/**
+ * Adds click event to each individual item on favourite list to toggle the note input for that hero.
+ * @param {array} heroIds complete list of hero IDs
+ */
+const heroNoteToggler = (heroIds) => {
+	heroIds.forEach((id) => {
+		$(`#${id}-toggler`).click(() => {
+			$(`#${id}`).toggle()
+		})
 	})
+}
+
+// Toasts
+$(".toast").toast("show")
+
+$(".toast-close").click(() => {
+	$(".toast").toast("hide")
 })
 
 $(document).ready(function () {
+	heroNoteToggler(heroIdList)
+
 	$(document).click(function () {
 		$(".navbar-collapse").collapse("hide")
 	})
